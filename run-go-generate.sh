dab7921af529afab05771f39eb4ee41b5cd33838
Tyler Gillson <tyler.gillson@gmail.com>
2022-11-23 18:45:00 -0800

    Update run-go-generate.sh

--- run-go-generate.sh
+++ run-go-generate.sh
@@ -4,13 +4,14 @@ git status >&2
 
 pre=$(git diff-index HEAD)
 
-echo "$@" | xargs -n1 go generate >&2
+go generate >&2
 
 git status >&2
 
 post=$(git diff-index HEAD)
 
 echo "PRE: $pre" >&2
+echo >&2
 echo "POST: $post" >&2
 
 if ! [[ "$pre" == "$post" ]]; then
