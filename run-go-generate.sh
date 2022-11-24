993b3b17e971580d2d9e154d9e34f5ba4909b691
Tyler Gillson <tyler.gillson@gmail.com>
2022-11-23 18:47:21 -0800

    Update run-go-generate.sh

--- run-go-generate.sh
+++ run-go-generate.sh
@@ -1,20 +1,3 @@
 #!/usr/bin/env bash
 
-git status >&2
-
-pre=$(git diff-index HEAD)
-
-go generate >&2
-
-git status >&2
-
-post=$(git diff-index HEAD)
-
-echo "PRE: $pre" >&2
-echo >&2
-echo "POST: $post" >&2
-
-if ! [[ "$pre" == "$post" ]]; then
-  echo "You need to run `go generate`" >&2
-  exit 1
-fi
+go generate
